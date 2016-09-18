a85b1a518ecb6b0033b2c1b10f54b43f23df7e20
Michael Sainz <msainz@tableau.com>
2016-09-17 22:35:23 -0700

    Added DownloadArtifact.psd1 file. Added some verbosity to the script.

---
+++ DownloadArtifacts.psd1
@@ -0,0 +1,92 @@
+@{
+    VCRedist2k5_x86 = @{
+        FullName = "Microsoft Visual C++ 2005 x86 Redistributable";
+        ShortName = "VCRedist2k5_x86";
+        Url = "http://download.microsoft.com/download/d/3/4/d342efa6-3266-4157-a2ec-5174867be706/vcredist_x86.exe";
+        CommandLine = "vcredist_x86.exe /s"
+    }
+        VCRedist2k5_x64 = @{
+        FullName = "Microsoft Visual C++ 2005 x64 Redistributable";
+        ShortName = "VCRedist2k5_x64";
+        Url = "http://download.microsoft.com/download/9/1/4/914851c6-9141-443b-bdb4-8bad3a57bea9/vcredist_x64.exe";
+        CommandLine = "vcredist_x64.exe /s"
+    }
+        VCRedist2k8_x86 = @{
+        FullName = "Microsoft Visual C++ 2008 x86 Redistributable";
+        ShortName = "VCRedist2k8_x86";
+        Url = "http://download.microsoft.com/download/1/1/1/1116b75a-9ec3-481a-a3c8-1777b5381140/vcredist_x86.exe";
+        CommandLine = "vcredist_x86.exe /s"
+    }
+        VCRedist2k8_x64 = @{
+        FullName = "Microsoft Visual C++ 2008 x64 Redistributable";
+        ShortName = "VCRedist2k8_x64";
+        Url = "http://download.microsoft.com/download/d/2/4/d242c3fb-da5a-4542-ad66-f9661d0a8d19/vcredist_x64.exe";
+        CommandLine = "vcredist_x64.exe /s"
+    }
+        VCRedist2k10_x86 = @{
+        FullName = "Microsoft Visual C++ 2010 x86 Redistributable";
+        ShortName = "VCRedist2k10_x86";
+        Url = "http://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe";
+        CommandLine = "vcredist_x86.exe /s"
+    }
+        VCRedist2k10_x64 = @{
+        FullName = "Microsoft Visual C++ 2010 x64 Redistributable";
+        ShortName = "VCRedist2k10_x64";
+        Url = "http://download.microsoft.com/download/3/2/2/3224B87F-CFA0-4E70-BDA3-3DE650EFEBA5/vcredist_x64.exe";
+        CommandLine = "vcredist_x64.exe /s"
+    }
+        VCRedist2k12_x86 = @{
+        FullName = "Microsoft Visual C++ 2012 x86 Redistributable";
+        ShortName = "VCRedist2k12_x86";
+        Url = "http://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe";
+        CommandLine = "vcredist_x86.exe /s"
+    }
+        VCRedist2k12_x64 = @{
+        FullName = "Microsoft Visual C++ 2012 x64 Redistributable";
+        ShortName = "VCRedist2k12_x64";
+        Url = "http://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe";
+        CommandLine = "vcredist_x64.exe /s"
+    }
+        VCRedist2k13_x86 = @{
+        FullName = "Microsoft Visual C++ 2013 x86 Redistributable";
+        ShortName = "VCRedist2k13_x86";
+        Url = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe";
+        CommandLine = "vcredist_x86.exe /s"
+    }
+        VCRedist2k13_x64 = @{
+        FullName = "Microsoft Visual C++ 2013 x64 Redistributable";
+        ShortName = "VCRedist2k13_x64";
+        Url = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe";
+        CommandLine = "vcredist_x64.exe /s"
+    }
+        VCRedist2k15_x86 = @{
+        FullName = "Microsoft Visual C++ 2015 x86 Redistributable";
+        ShortName = "VCRedist2k15_x86";
+        Url = "http://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x86.exe";
+        CommandLine = "vcredist_x86.exe /s"
+    }
+        VCRedist2k15_x64 = @{
+        FullName = "Microsoft Visual C++ 2015 x64 Redistributable";
+        ShortName = "VCRedist2k15_x64";
+        Url = "http://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe";
+        CommandLine = "vcredist_x64.exe /s"
+    }
+        "NetFx4-6-2_x64" = @{
+        FullName = ".Net Framework 4.6.2";
+        ShortName = "NetFx4-6-2_x64";
+        Url = "http://download.microsoft.com/download/1/7/5/175E764B-E417-4FBB-95DF-62676FC7B2EA/NDP462-KB3120735-x86-x64-AllOS-ENU.exe";
+        CommandLine = "NDP462-KB3120735-x86-x64-AllOS-ENU.exe /s"
+    }
+        MDT2013U2 = @{
+        FullName = "Microsoft Deployment Toolkit 2013 Update 2";
+        ShortName = "MDT2013U2";
+        Url = "http://download.microsoft.com/download/3/0/1/3012B93D-C445-44A9-8BFB-F28EB937B060/MicrosoftDeploymentToolkit2013_x64.msi";
+        CommandLine = "msiexec.exe /i MicrosoftDeploymentToolkit2013_x64.msi /qn"
+    }
+        ADKv1607 = @{
+        FullName = "Automated Installation Kit v1607";
+        ShortName = "ADKv1607";
+        Url = "http://download.microsoft.com/download/9/A/E/9AE69DD5-BA93-44E0-864E-180F5E700AB4/adk/adksetup.exe";
+        CommandLine = "adksetup.exe /qn"
+    }
+}
--- Script.ps1
+++ Script.ps1
@@ -1,14 +1,22 @@
 <#
 .Synopsis
-   Short description
+   Script automatically installs and configures an MDT deployment share and
+   imports common packages.
 .DESCRIPTION
    This script downloads, installs and configures the Microsoft Deployment
-   Toolkit 2013 Update 2, Automated Deployment Toolkit and commmon applications
-   to the Deployment Share.
+   Toolkit 2013 Update 2, Automated Deployment Toolkit and imports the 
+   Microsoft Visual C++ binaries that are common for image creation.
 .EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
+   .\MDTDownloader.ps1 -DownloadLocation C:\Downloads -DSLocation C:\DS
+   -DSNetworkPath "\\MyComputer\MDTProduction$"
+.PARAMETER DownloadLocation
+   Specifies the location to download files for use in the creation of the
+   MDT deployment share. Default location is "$env:Temp".
+.PARAMETER DSLocation
+   Specifies the location of the MDT deployment share.
+.PARAMETER DSNetworkPath
+   Specifies the network UNC path for the MDT deployment share. Default value
+   is "$env:ComputerName\MDTProduction$"
 #>
 
 [CmdletBinding()]
@@ -28,47 +36,76 @@ Param (
     # The UNC path of the deployment share
     [Parameter(Mandatory=$false,
                 Position=2,
-                HelpMessage='The UNC path of the SMB share. Default is $ComputerName\MDTProduction$')]
+                HelpMessage='The UNC path of the SMB share. Default is LOCALHOST\MDTProduction$')]
     [String]$DSNetworkPath = "$env:ComputerName\MDTProduction$"
 )
-If (-not (Test-Path -Path "$DownloadLocation\MicrosoftDeploymentToolkit2013_x64.msi")) {
-    Write-Verbose -Message 'Downloading the Microsoft Deployment Toolkit'
-    Invoke-WebRequest -Uri 'https://download.microsoft.com/download/3/0/1/3012B93D-C445-44A9-8BFB-F28EB937B060/MicrosoftDeploymentToolkit2013_x64.msi' -Outfile "$DownloadLocation\MicrosoftDeploymentToolkit2013_x64.msi"
+
+Write-Debug -Message 'Creating hashtable of download artifacts and URLs'
+$DownLoadArtifacts = Import-PowerShellDataFile -Path .\DownloadArtifacts.psd1
+
+Write-Verbose -Message "Downloading all artifacts"
+Foreach ($PSItem in $DownLoadArtifacts.GetEnumerator()) {
+    Write-Debug -Message "Current item is: $($PSItem.Key)"
+    Start-Job -Name "$($PSItem.Name)" -ArgumentList $PSItem,$DownloadLocation -ScriptBlock {
+        $DownloadArtifact = $args[0]
+        Write-Debug -Message "Current value of DownloadArtifact Key is: $($DownloadArtifact.Key)"
+        Write-Debug -Message "Current Value of DownloadArtifact Value is: $($DownloadArtifact.Value)"
+        $DownloadLocation = $args[1]
+
+        Write-Debug -Message "Creating directory for download"
+        New-Item -Path "$DownloadLocation" -Name "$($DownloadArtifact.Key)" -ItemType Directory
+        Write-Debug -Message "Executing line: $DownloadLocation\$($DownloadArtifact.Key)\$(Split-Path -Path $DownloadArtifact.Value -Leaf)"
+        Invoke-WebRequest -Uri "$($($DownloadArtifact.Value).Url)" -Outfile "$DownloadLocation\$($DownloadArtifact.Key)\$(Split-Path -Path $($($DownloadArtifact.Value).Url) -Leaf)"
+        } | Out-Null
+}
+Write-Verbose -Message "Waiting for download artifact jobs to finish"
+Get-Job | Wait-Job | Out-Null
+
+Write-Debug -Message 'Checking to see if MDT is already installed'
+If (-not (Get-CimInstance -ClassName 'Win32_Product' -Filter "IdentifyingNumber='{F172B6C7-45DD-4C22-A5BF-1B2C084CADEF}'")) {
+    Write-Debug -Message 'MDT is not currently installed'
+    Write-Verbose -Message 'Installing Microsoft Deployment Toolkit'
+    Start-Process -FilePath "$env:SystemRoot\System32\msiexec.exe" -ArgumentList "/i $DownloadLocation\MDT2013U2\MicrosoftDeploymentToolkit2013_x64.msi /qn" -Wait
 }
 Else {
-    Write-Verbose -Message 'MDT installer already exists, skipping download'
+    Write-Debug -Message 'MDT is already installed'
 }
 
-If (-not (Test-Path -Path "$DownloadLocation\adksetup.exe")) {
-    Write-Verbose -Message 'Downlading the Automated Deployment Kit v1607'
-    Invoke-WebRequest -Uri 'http://download.microsoft.com/download/9/A/E/9AE69DD5-BA93-44E0-864E-180F5E700AB4/adk/adksetup.exe' -Outfile "$DownloadLocation\adksetup.exe"
+Write-Debug -Message 'Checking to see if the ADK is already installed'
+If (-not (Get-CimInstance -ClassName 'Win32_Product' -Filter "IdentifyingNumber='{52EA560E-E50F-DC8F-146D-1B631548BA29}'")) {
+    Write-Debug -Message 'The ADK is not currently installed'
+    Write-Verbose -Message 'Installing the Automated Deployment Kit'
+    Start-Process -FilePath "$DownloadLocation\ADKv1607\adksetup.exe" -ArgumentList '/Features OptionId.DeploymentTools OptionId.WindowsPreinstallationEnvironment OptionId.UserStateMigrationTool /norestart /quiet /ceip off' -Wait
+
 }
 Else {
-    Write-Verbose -Message 'ADK installer already exists, skipping download'
+    Write-Debug -Message 'The ADK is already installed'
 }
-Write-Verbose -Message 'Installing Microsoft Deployment Toolkit'
-Start-Process -FilePath "$env:SystemRoot\System32\msiexec.exe" -ArgumentList "/i $DownloadLocation\MicrosoftDeploymentToolkit2013_x64.msi /qn" -Wait
-Write-Verbose -Message 'Installing the Automated Deployment Kit'
-Start-Process -FilePath "$DownloadLocation\adksetup.exe" -ArgumentList '/Features OptionId.DeploymentTools OptionId.WindowsPreinstallationEnvironment OptionId.UserStateMigrationTool /norestart /quiet /ceip off' -Wait
 
 Add-PSSnapin -Name Microsoft.BDD.PSSnapIn
 Import-Module -Name "$env:ProgramFiles\Microsoft Deployment Toolkit\Bin\MicrosoftDeploymentToolkit.psd1"
 
 Write-Verbose -Message "Creating MDT deployment share at $DSLocation"
-If (-not $DSLocation) {
+If (-not (Test-Path -Path $DSLocation)) {
     Write-Debug -Message "$DSLocation does not exist, creating folder."
-    New-Item -Path $DSLocation -ItemType Directory
-    New-PSDrive -Name "DS001" -PSProvider "MDTProvider" -Root $DSLocation -Description "MDTProduction" -NetworkPath "\\$env:ComputerName\MDTProduction$"
+    New-Item -Path $DSLocation -ItemType Directory  | Out-Null
+    New-PSDrive -Name "DS001" -PSProvider "MDTProvider" -Root $DSLocation -Description "MDTProduction" -NetworkPath $DSNetworkPath | Out-Null
 }
 Else {
-    Write-Debug -Message "$DSLocation already exists"
-    New-PSDrive -Name "DS001" -PSProvider "MDTProvider" -Root $DSLocation -Description "MDTProduction" -NetworkPath "\\$env:ComputerName\MDTProduction$"
+    Write-Debug -Message "$DSLocation already exists, checking if it is empty"
+    If ((Get-ChildItem -Path $DSLocation | Measure-Object).Count -eq 0) {
+        New-PSDrive -Name "DS001" -PSProvider "MDTProvider" -Root $DSLocation -Description "MDTProduction" -NetworkPath $DSNetworkPath | Out-Null
+    }
 }
 
-New-Item -Path "DS001:\Applications" -Name Microsoft -ItemType Directory
-New-Item -Path "DS001:\Applications" -Name Other -ItemType Directory
-New-Item -Path "DS001:\Operating Systems" -Name Build -ItemType Directory
-New-Item -Path "DS001:\Operating Systems" -Name Deploy -ItemType Directory
-New-Item -Path "DS001:\Task Sequences" -Name Build -ItemType Directory
-New-Item -Path "DS001:\Task Sequences" -Name Deploy -ItemType Directory
+New-Item -Path "DS001:\Applications" -Name Microsoft -ItemType Directory | Out-Null
+New-Item -Path "DS001:\Applications" -Name Other -ItemType Directory | Out-Null
+New-Item -Path "DS001:\Operating Systems" -Name Build -ItemType Directory | Out-Null
+New-Item -Path "DS001:\Operating Systems" -Name Deploy -ItemType Directory | Out-Null
+New-Item -Path "DS001:\Task Sequences" -Name Build -ItemType Directory | Out-Null
+New-Item -Path "DS001:\Task Sequences" -Name Deploy -ItemType Directory | Out-Null
 
+Foreach ($i in $DownLoadArtifacts.GetEnumerator()) {
+    Write-Debug -Message "Creating application: $($i.Key)"
+    Import-MDTApplication -Path "DS001:\Applications\Microsoft" -Name $i.Key -ApplicationSourcePath "$DownloadLocation\$($i.Key)" -DestinationFolder $i.Key -CommandLine (Split-Path -Path $i.Value -Leaf) -ShortName $i.Key | Out-Null
+}
